3c0d982afb3eeef965421a98f7f5bc2f76728ad4
Thalisson Braga <thalisson.braga1028@gmail.com>
2025-03-17 14:06:54 -0300

    Added the table visualization functions

--- Flashcards/TableVisualizationEngine.cs
+++ Flashcards/TableVisualizationEngine.cs
@@ -1,13 +1,42 @@
-﻿using Flashcards.Models;
+﻿using ConsoleTableExt;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using Flashcards.Models;
+using Flashcards.Models.DTO;
 
 namespace Flashcards {
     internal class TableVisualizationEngine {
         internal static void PrepareFlashcardList(int id, List<FlashcardWithStack> cards) {
-            throw new NotImplementedException();
+            string stackName = cards.FirstOrDefault().StackName;
+            string tableName = $"{id} - {stackName}";
+
+            List<StacksFlashcardsView> stackToView = new List<StacksFlashcardsView>();
+            int cardIndex = 1;
+            cards.ForEach(x => {
+                stackToView.Add(new StacksFlashcardsView {
+                    Id = cardIndex,
+                    Question = x.Question,
+                    Answer = x.Answer
+                });
+                cardIndex++;
+            });
+            ShowTable(stackToView, tableName);
         }
 
-        internal static void ShowTable(List<Stack> stacks, object value) {
-            throw new NotImplementedException();
+        internal static void ShowTable<T>(List<T> stacks, [AllowNull]string tableName) where T : class{
+            if(tableName == null) {
+                tableName = "";
+            }
+
+            Console.WriteLine("\n\n");
+
+            ConsoleTableBuilder
+                  .From(stacks)
+                  .WithTitle(tableName)
+                  .ExportAndWriteLine();
+            Console.WriteLine("\n\n");
         }
     }
 }
